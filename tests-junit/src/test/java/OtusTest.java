ef142a9dedcf4811d0b795d05c7298fea851f091
Andrey <alistert@yandex.ru>
2020-05-04 16:09:22 +0300

    add open otus test

---
+++ tests-junit/src/test/java/OtusTest.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class OtusTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://otus.ru/");
+        Thread.sleep(5000);
+
+
+    }
+
+}
